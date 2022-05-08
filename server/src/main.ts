d1c200c053ef8dbf3461e16f8241459fb6d81f83
PierreHervelin <hervelinp.code@gmail.com>
2022-05-08 22:27:43 +0200

    [func] add helmet

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,9 +3,11 @@ import { AppModule } from './app.module';
 import * as bodyParser from 'body-parser';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.use(helmet());
     app.enableCors();
     app.use(bodyParser.json({ limit: '50mb' }));
     app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
