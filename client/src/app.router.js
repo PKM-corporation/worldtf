fbf8e9f8dfe0d988a14318e35d4cae076ba12383
hophelie <hophelie@twiiky-corp.fr>
2022-05-08 19:07:05 +0200

    #38 : Model integration (Login - register)

--- client/src/app.router.js
+++ client/src/app.router.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import GamePage from './pages/game.page';
+import AuthenticatePage from './pages/authenticate.page';
 
 const AppRouter = () => {
     return (
         <Routes>
             <Route path="/" element={<GamePage />} />
+            <Route path="/authenticate" element={<AuthenticatePage />} />
         </Routes>
     );
 };
