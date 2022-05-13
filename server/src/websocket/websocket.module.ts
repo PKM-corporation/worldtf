8c2f9b4be19b3efea425f553569a26432eb6c5b0
PierreHervelin <hervelinp.code@gmail.com>
2022-05-13 06:59:22 +0200

    [func] #44 websocket controller

--- server/src/websocket/websocket.module.ts
+++ server/src/websocket/websocket.module.ts
@@ -1,10 +1,12 @@
 import { CacheModule, Module } from '@nestjs/common';
 import { AuthModule } from 'src/auth/auth.module';
+import { WebsocketController } from './websocket.controller';
 import { WebsocketGateway } from './websocket.gateway';
 import { WebsocketService } from './websocket.service';
 
 @Module({
     imports: [CacheModule.register(), AuthModule],
+    controllers: [WebsocketController],
     providers: [WebsocketService, WebsocketGateway],
 })
 export class WebsocketModule {}
