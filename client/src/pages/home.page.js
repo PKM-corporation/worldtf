bbec3feaee9fd13b06e91c180a4788730fc1b16c
PierreHervelin <hervelinp.code@gmail.com>
2022-05-25 15:27:06 +0200

    [fix] dependencie in useEffect in home page

--- client/src/pages/home.page.js
+++ client/src/pages/home.page.js
@@ -19,7 +19,7 @@ const HomePage = () => {
         getConnectedPlayers().then((data) => {
             setConnectedPlayers(data.onlineClients);
         });
-    });
+    }, []);
 
     return (
         <div className="home">
