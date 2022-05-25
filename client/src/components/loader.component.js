9a226b9140d0dd82d70b282a11009229f23298e5
hophelie <hophelie@twiiky-corp.fr>
2022-05-25 16:27:32 +0200

    [ funct ] #77 loader-style

--- client/src/components/loader.component.js
+++ client/src/components/loader.component.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import loader from '../assets/loader.svg';
 
 const LoaderComponent = () => {
-    return <div className="loader">Loading...</div>;
+    return (
+        <div className="loader">
+            <p className="loader-text">Chargement...</p>
+            <img src={loader} alt="Chargement..." />
+        </div>
+    );
 };
 
 export default LoaderComponent;
